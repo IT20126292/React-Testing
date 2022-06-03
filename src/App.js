9421f5d561cc7cfda55763c4a9a2bd562c98dc7b
Kavisha Silva <it20126292@my.sliit.lk>
2022-06-04 01:30:31 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Blog from './Components/Blog';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Project from './Components/Project';
+import Service from './Components/Service';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Home/>
+        <About/>
+        <Service/>
+        <Project/>
+        <Blog/>
+        <Contact/>
+        <Footer/>
     </div>
   );
 }
