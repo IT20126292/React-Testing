0527c3538da8f369e6d5e653a47a8baeb2a3e56d
Kavisha Silva <it20126292@my.sliit.lk>
2022-06-07 18:38:32 +0530

    add personal info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
         header.classList.toggle("active", window.scrollY > 0);
     });
     // Toogle Menu
-    const [show, setShow] = useState(true);
+    const [show, setShow] = useState(false);
     
     return (
         <div className="home" id="Home">
@@ -44,38 +44,38 @@ export default function Home() {
               <div className="toggle__menu">
                 <svg onClick={() => setShow(!show)}
                   xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
+                  width="18"
+                  height="18"
                   fill="currentColor"
-                  class="bi bi-justify white pointer"
+                  className="bi bi-justify white pointer"
                   viewBox="0 0 16 16"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
                   />
                 </svg>
               </div>
               {show ?(
-              <div className="sideNavbar">
+              <div style={{textAlign:'center'}} className="sideNavbar">
                   <ul className="sidebar d__flex">
                   <li className="sideNavbar">
-                  <a href="#home">Home</a>
+                  <a href="#Home">Home</a>
                 </li>
                 <li className="sideNavbar">
-                  <a href="#about">About</a>
+                  <a href="#About">About</a>
                 </li>
                 <li className="sideNavbar">
-                  <a href="#services">Services</a>
+                  <a href="#Services">Services</a>
                 </li>
                 <li className="sideNavbar">
-                  <a href="#portfolio">Portfolio</a>
+                  <a href="#Portfolio">Portfolio</a>
                 </li>
                 <li className="sideNavbar">
-                  <a href="#blog">Blog</a>
+                  <a href="#Blog">Blog</a>
                 </li>
                 <li className="sideNavbar">
-                  <a href="#contact">Contact</a>
+                  <a href="#Contact">Contact</a>
                 </li>
                   </ul>
               </div>
@@ -86,9 +86,9 @@ export default function Home() {
               <div className="home__content">
                 <div className="home__meta">
                   <h1 className="home__text pz__10">WELCOME TO MY WORLD</h1>
-                  <h2 className="home__text pz__10">Hi, I’m Jone Doe</h2>
-                  <h3 className="home__text sweet pz__10">JS Developer.</h3>
-                  <h4 className="home__text pz__10">based in USA.</h4>
+                  <h2 className="home__text pz__10">Hi, I’m Kavisha Silva</h2>
+                  <h3 className="home__text sweet pz__10">Full Stack Developer.</h3>
+                  <h4 className="home__text pz__10">based in Sri Lanka.</h4>
                 </div>
               </div>
             </div>
